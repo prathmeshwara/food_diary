08959562c1ec2fcdecb8ef7d0142ac301684da63
Prathmeshwara <prathmeshwara@gmail.com>
2013-05-31 05:23:52 +0530

    Branch: master
        When no image is associated with a Diet logged a default image
        is rendered by Paperclip.The default image was displayed in its
        full dimension which was distorting the view.Fixed the same.

--- app/helpers/diets_helper.rb
+++ app/helpers/diets_helper.rb
@@ -3,4 +3,13 @@ module DietsHelper
     return '' unless diet.present?
     diet.created_at.strftime('%b %d, %Y %H:%M:%S %:z')
   end
+
+  def diet_photo(diet, style=:medium)
+    if diet.photo.blank?
+      image_tag @diet.photo.url, size: '250x150'
+    else
+      image_tag @diet.photo.url(style)
+    end
+  end
+
 end
